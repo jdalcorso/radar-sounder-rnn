cb0ff32ba09dfae4976cdb4f0617a9f77c61382c
Jordy Dal Corso <jordy.dalcorso@gmail.com>
2024-09-24 15:34:36 +0000

    minor changes to model. Now works

--- src/model.py
+++ src/model.py
@@ -3,16 +3,17 @@ from cnn import Encoder, Decoder
 from rnn import ConvLSTM, ConvRNN
 
 class Model(nn.Module):
-    def __init__(self, in_channels = 1, out_channels = 5):
+    def __init__(self, in_channels, hidden_channels, out_channels, n_layers, hidden_scaling):
         super().__init__()
-        self.encoder = Encoder()
-        self.decoder = Decoder()
-        self.rnn = ConvLSTM(in_channels=128, out_channels=128)
+        self.encoder = Encoder(in_channels, hidden_channels, n_layers, hidden_scaling)
+        self.decoder = Decoder(hidden_channels, out_channels, n_layers, hidden_scaling)
+        self.rnn = ConvLSTM(hidden_channels, hidden_channels)
         self.out_channels = out_channels
+        self.nparams = sum(p.numel() for p in self.parameters() if p.requires_grad)
 
     def forward(self, x): # BTcHW
         outputs = []
-        B,T,c,H,W = x.shape
+        B,T,c,H,W = x.shape # c=1
         x_enc = self.encoder(x.view(-1,c,H,W)) # (BT)Chw
         _,C,h,w = x_enc.shape # BTChw
         x_enc = x_enc.view(B,T,C,h,w) # BTChw
