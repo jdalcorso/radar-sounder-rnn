25409f40a9a688ca1d5de49b4dcd7b7af3ce1fdf
Jordy Dal Corso <jordy.dalcorso@gmail.com>
2025-02-21 17:22:11 +0000

    working cycle function. Weighting on Lcc is now 1/seq_len. This seem preventing learning the identity

--- scripts/cycle.py
+++ scripts/cycle.py
@@ -63,13 +63,14 @@ def main(
     optimizer = AdamW(model.parameters(), lr)
 
     # Train
-    loss_train_tot = []
+    loss_train_tot_sup = []
+    loss_train_tot_cycle = []
     loss_val_tot = []
     for epoch in range(epochs):
         # Train
         model.train(True)
         t0 = time.time()
-        seq, label, pred, loss_train = train(
+        seq, label, pred, loss_train_sup, loss_train_cycle = train(
             model, optimizer, train_dl, seq_len, ce_weights, pos_enc
         )
         t1 = time.time() - t0
@@ -93,27 +94,39 @@ def main(
                 out_dir + "/val" + str(epoch + 1) + ".png",
             )
 
-        loss_train, loss_val = (
-            torch.tensor(loss_train).mean(),
+        loss_train_sup, loss_train_cycle, loss_val = (
+            torch.tensor(loss_train_sup).mean(),
+            torch.tensor(loss_train_cycle).mean(),
             torch.tensor(loss_val).mean(),
         )
-        loss_train_tot.append(loss_train)
+        loss_train_tot_sup.append(loss_train_sup)
+        loss_train_tot_cycle.append(loss_train_cycle)
         loss_val_tot.append(loss_val)
-        loss_train_show = [(lt / loss_train_tot[0]).item() for lt in loss_train_tot]
+        loss_train_show = [
+            ((lt[0] + lt[1]) / (loss_train_tot_sup[0] + loss_train_tot_cycle[0])).item()
+            for lt in zip(loss_train_tot_sup, loss_train_tot_cycle)
+        ]
         loss_val_show = [(lt / loss_val_tot[0]).item() for lt in loss_val_tot]
         plot_loss(loss_train_show, loss_val_show, out_dir)
 
-        logger_str = "Epoch: {}, Loss train: {:.3f}, Loss val: {:.3f}, Time: {:.3f}"
+        logger_str = "Epoch: {}, Loss sup: {:.3f}, Loss cyc: {:.3f}, Loss val: {:.3f}, Time: {:.3f}"
         logger.info(
-            logger_str.format(epoch + 1, loss_train.item(), loss_val.item(), t1)
+            logger_str.format(
+                epoch + 1,
+                loss_train_sup.item(),
+                loss_train_cycle.item(),
+                loss_val.item(),
+                t1,
+            )
         )
 
     torch.save(model.state_dict(), out_dir + "/latest.pt")
 
 
-@torch.compile
+# @torch.compile
 def train(model, optimizer, dataloader, seq_len, ce_weights, pos_enc):
-    loss_train = []
+    loss_train_sup = []
+    loss_train_cycle = []
     ce_weights = torch.tensor(ce_weights, device="cuda")
     for _, item in enumerate(dataloader):
         seq = item[0].to("cuda").unsqueeze(2)  # BTHW -> BTCHW
@@ -123,33 +136,36 @@ def train(model, optimizer, dataloader, seq_len, ce_weights, pos_enc):
         preds = []
         hidden, cell = None, None
         optimizer.zero_grad()
-        loss = 0
-        for i in range(seq_len * 2):
+        loss_sup = 0
+        loss_cycle = 0
+        for i in range(seq_len * 2):  # Cycle through the sequence and its reverse
             pred, hidden, cell = model(seq[:, i], hidden, cell)  # BCHW
-            preds.append(pred.unsqueeze(1))  # B1CHW * T
-            # if i == 0:
-            #     loss += cross_entropy(pred, label[:, 0], ce_weights)
-            if i >= seq_len and i != seq_len * 2 - 1:
-                loss += ((seq_len * 2) * 0.1 - i * 0.1 + 0.1) * cross_entropy(
-                    pred,
-                    softmax(
-                        torch.flip(preds[2 * seq_len - i - 1].squeeze(1), dims=(-1,)),
+            preds.append(pred.unsqueeze(1))  # B1CHW (* T)
+            if i == 0:
+                loss_sup += cross_entropy(pred, label[:, 0], ce_weights)
+            if i >= seq_len + 2 and i != seq_len * 2 - 1:
+                loss_cycle += (1 / seq_len) * cross_entropy(
+                    torch.flip(pred, dims=(-1,)),  # BCHW
+                    softmax(  # TODO: check whether to use argmax
+                        preds[2 * seq_len - i - 1].squeeze(1),
                         dim=1,
                     ),
                     ce_weights,
                 )
             if i == seq_len * 2 - 1:
-                loss += cross_entropy(
+                loss_sup += cross_entropy(
                     pred,
                     torch.flip(label[:, 0], dims=(-1,)),
                     ce_weights,
                 )
 
-        loss_train.append(loss)
+        loss = loss_sup + loss_cycle
+        loss_train_sup.append(loss_sup)
+        loss_train_cycle.append(loss_cycle)
         loss.backward()
         optimizer.step()
     preds = torch.cat(preds, 1)  # BTCHW
-    return seq, label, preds, loss_train
+    return seq, label, preds, loss_train_sup, loss_train_cycle
 
 
 if __name__ == "__main__":
